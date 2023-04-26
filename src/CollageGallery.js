c972d65c74fde77c276eb5ad64faebddd03426e3
Josh Broughton <establishedwebsolutions@gmail.com>
2023-04-25 18:14:21 -0700

    Images display front end, need to implement SASS

--- src/CollageGallery.js
+++ src/CollageGallery.js
@@ -1,4 +1,61 @@
+// import React, { useState, useEffect } from 'react';
+
+// const CollageGallery = ({ images, fetchImages }) => {
+//   const [hasMoreImages, setHasMoreImages] = useState(true);
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       if (!hasMoreImages) return;
+
+//       if (
+//         window.innerHeight + document.documentElement.scrollTop >=
+//         document.documentElement.offsetHeight - 500
+//       ) {
+//         fetchImages(images.length);
+//       }
+//     };
+
+//     window.addEventListener('scroll', handleScroll);
+//     return () => window.removeEventListener('scroll', handleScroll);
+//   }, [images, hasMoreImages, fetchImages]);
+
+//   useEffect(() => {
+//     if (images.length === 0) {
+//       setHasMoreImages(true);
+//       fetchImages();
+//     } else if (images.length > 0 && images.length % 20 !== 0) {
+//       setHasMoreImages(false);
+//     }
+//   }, [images, fetchImages]);
+
+//   const collageGalleryStyles = {
+//     collageGallery: {
+//       display: 'grid',
+//       gridTemplateColumns: 'repeat(auto-fill, minmax(180px, 1fr))',
+//       gridAutoRows: '180px',
+//       gridGap: '10px',
+//     },
+//     img: {
+//       width: '100%',
+//       height: '100%',
+//       objectFit: 'cover',
+//     },
+//   };
+
+//   return (
+//     <div style={collageGalleryStyles.collageGallery}>
+//       {images.map((image) => (
+//         <img key={image.id} src={image.url} alt="" style={collageGalleryStyles.img} />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default CollageGallery;
+
+
 import React, { useState, useEffect } from 'react';
+import Masonry from 'react-masonry-css';
 
 const CollageGallery = ({ images, fetchImages }) => {
   const [hasMoreImages, setHasMoreImages] = useState(true);
@@ -28,12 +85,23 @@ const CollageGallery = ({ images, fetchImages }) => {
     }
   }, [images, fetchImages]);
 
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
+
   return (
-    <div>
+    <Masonry
+      breakpointCols={breakpointColumnsObj}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column"
+    >
       {images.map((image) => (
-        <img key={image.id} src={image.url} alt="" />
+        <img key={image.id} src={image.url} alt="" style={{ width: '100%' }} />
       ))}
-    </div>
+    </Masonry>
   );
 };
 
