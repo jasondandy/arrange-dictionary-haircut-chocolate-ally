6fffba8ad17a271b5d860b005624adf53d77940d
Josh Broughton <establishedwebsolutions@gmail.com>
2023-04-28 11:41:09 -0700

    Cleaned

--- src/CollageGallery.js
+++ src/CollageGallery.js
@@ -1,58 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-
-// const CollageGallery = ({ images, fetchImages }) => {
-//   const [hasMoreImages, setHasMoreImages] = useState(true);
-
-//   useEffect(() => {
-//     const handleScroll = () => {
-//       if (!hasMoreImages) return;
-
-//       if (
-//         window.innerHeight + document.documentElement.scrollTop >=
-//         document.documentElement.offsetHeight - 500
-//       ) {
-//         fetchImages(images.length);
-//       }
-//     };
-
-//     window.addEventListener('scroll', handleScroll);
-//     return () => window.removeEventListener('scroll', handleScroll);
-//   }, [images, hasMoreImages, fetchImages]);
-
-//   useEffect(() => {
-//     if (images.length === 0) {
-//       setHasMoreImages(true);
-//       fetchImages();
-//     } else if (images.length > 0 && images.length % 20 !== 0) {
-//       setHasMoreImages(false);
-//     }
-//   }, [images, fetchImages]);
-
-//   const collageGalleryStyles = {
-//     collageGallery: {
-//       display: 'grid',
-//       gridTemplateColumns: 'repeat(auto-fill, minmax(180px, 1fr))',
-//       gridAutoRows: '180px',
-//       gridGap: '10px',
-//     },
-//     img: {
-//       width: '100%',
-//       height: '100%',
-//       objectFit: 'cover',
-//     },
-//   };
-
-//   return (
-//     <div style={collageGalleryStyles.collageGallery}>
-//       {images.map((image) => (
-//         <img key={image.id} src={image.url} alt="" style={collageGalleryStyles.img} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default CollageGallery;
-
 import React, { useState, useEffect } from "react";
 import Masonry from "react-masonry-css";
 
@@ -60,16 +5,6 @@ const CollageGallery = ({ images, fetchImages }) => {
     const [hasMoreImages, setHasMoreImages] = useState(true);
 
     useEffect(() => {
-        // const handleScroll = () => {
-        //   if (!hasMoreImages) return;
-
-        //   if (
-        //     window.innerHeight + document.documentElement.scrollTop >=
-        //     document.documentElement.offsetHeight - 500
-        //   ) {
-        //     fetchImages(images.length);
-        //   }
-        // };
 
         const handleScroll = () => {
             if (!hasMoreImages) return;
